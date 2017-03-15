a7869d07629c62eb45d5a9a4d033fd35b2e8cc65
chris ward <ward.christopherj@gmail.com>
2017-03-15 16:42:03 -0700

    upload file

--- js/hw.js
+++ js/hw.js
@@ -1,51 +1,51 @@
-
+var animalButtons = [
+  "dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla", "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken", "capybara", "teacup pig", "serval", "salamander", "frog" 
+];
 
 $("button").on("click", function() {
-      // In this case, the "this" keyword refers to the button that was clicked
       var topics = $(this).attr("animalButtons");
 
-      // Constructing a URL to search Giphy for the name of the person who said the quote
       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
         topics + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-      // Performing our AJAX GET request
       $.ajax({
           url: queryURL,
           method: "GET"
         })
-        // After the data comes back from the API
         .done(function(response) {
-          // Storing an array of results in the results variable
           var results = response.data;
 
-          // Looping over every result item
           for (var i = 0; i < results.length; i++) {
 
-            // Only taking action if the photo has an appropriate rating
             if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-              // Creating a div with the class "item"
               var gifDiv = $("<div class='item'>");
 
-              // Storing the result item's rating
               var rating = results[i].rating;
 
-              // Creating a paragraph tag with the result item's rating
               var p = $("<p>").text("Rating: " + rating);
 
-              // Creating an image tag
               var topicsImage = $("<img>");
-
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
               topicsImage.attr("src", results[i].images.fixed_height.url);
-
-              // Appending the paragraph and personImage we created to the "gifDiv" div we created
+              // topicsImage.attr("data-still", results[i].images.fixed_height.url);
+              // topicsImage.attr("data-animate", results[i].images.fixed_height.url);
+              // topicsImage.attr("data-state", still);
+              // topicsImage.addclass(gif);
               gifDiv.append(p);
               gifDiv.append(topicsImage);
-
-              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
               $("#animals").prepend(gifDiv);
             }
           }
         });
+
+      $(".gif").on("click", function() {
+      var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
     });
+    });
+
